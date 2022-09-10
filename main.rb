c18642f69e39fcd5e90e5bd7fc1cfb1f67659c80
hipale <hhemmhilb@gmail.com>
2022-09-10 20:07:59 +0200

    Finish find method Change the insert method Start delete method

--- main.rb
+++ main.rb
@@ -32,8 +32,7 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
-  def insert(value, node = @root, starts = 0, ends = @array.length - 1)
-    mid = starts + (ends - starts) / 2
+  def insert(value, node = @root)
     if node.right == nil && value > node.data
       node.right = Node.new(value)
       return
@@ -41,35 +40,50 @@ class Tree
       node.left = Node.new(value)
       return
     end
-    if @array[mid] > value
-      insert(value, node.left, starts, mid - 1)
-    elsif @array[mid] < value
-      insert(value, node.right, mid + 1, ends)
+    if value < node.data 
+      insert(value, node.left) 
+    elsif value > node.data 
+      insert(value, node.right)
     end
   end
 
-  def find(value, node = @root, starts = 0, ends = @array.length - 1)
-    mid = starts + (ends - starts) / 2
-    if value == @array[mid]
+  def delete(value, node = @root)
+    if node == nil
+      return false
+    end
+    if node.right.data == value
+      node.right = nil
+      return
+    elsif node.left.data == value
+      node.left = nil
+      return 
+    end
+    value < node.data ? delete(value, node.left) : delete(value, node.right)
+  end
+
+  def find(value, node = @root)
+    if node == nil
+      return false
+    end
+    if value == node.data
       return node
     end
-     if value > @array[mid]
-      find(value, node.right, mid + 1, ends)
-     elsif value < @array[mid]
-      find(value, node.left, starts, mid - 1)
-     end
+    value < node.data ? find(value, node.left) : find(value, node.right)
   end
 end
 
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 t = Tree.new(array)
-puts t.pretty_print
- t.insert(9999)
- t.insert(555)
- t.insert(8)
- t.insert(2)
+
+#puts t.pretty_print
+ #t.insert(9999)
+ #t.insert(555)
+ #t.insert(8)
+ #t.insert(2)
 
 puts t.pretty_print
 
-pp t.find(67)
+t.delete(6345)
+puts t.pretty_print
+#pp t.find(6345)
